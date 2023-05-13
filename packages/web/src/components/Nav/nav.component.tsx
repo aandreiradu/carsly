48bb5658d6f8d379b1ca425d2f246a41403b4c92
Andrei Radu <raduandrei697@gmail.com>
2023-05-13 23:28:47 +0300

    mobile nav

---
+++ packages/web/src/components/Nav/nav.component.tsx
@@ -0,0 +1,131 @@
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { List, X, User } from 'phosphor-react';
+import { SidebarLinkProps } from '../SidebarLink/sidebarlink.component';
+
+const navigationLinks: Omit<SidebarLinkProps, 'icon'>[] = [
+  {
+    label: 'Home',
+    isLink: true,
+    href: '/',
+    isActive: true,
+  },
+  {
+    label: 'My Profile',
+    isLink: true,
+    href: '/me',
+    isActive: false,
+  },
+  {
+    label: 'My Favorites',
+    isLink: true,
+    href: '/favorites',
+    isActive: false,
+  },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+const Nav = () => {
+  return (
+    <Disclosure as="nav" className="z-10 md:hidden bg-[#1f1f1f] w-full h-16">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <X className="block h-6 w-6" color="#fff" aria-hidden="true" />
+                  ) : (
+                    <List className="block h-6 w-6" color="#fff" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  {/* <img
+                    className="block h-8 w-auto lg:hidden"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  /> */}
+                  <h1 className="text-white font-kanit uppercase tracking-widest text-xl">carsly</h1>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-default-gray text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open user menu</span>
+                      <User className="h-8 w-8 rounded-full" color="#fff" />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2 bg-default-gray">
+              {navigationLinks.map((item) => (
+                <Disclosure.Button
+                  key={item.label}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.isActive
+                      ? 'bg-default-yellow text-black'
+                      : 'text-gray-300 hover:bg-default-yellow hover:text-black',
+                    'block rounded-md px-3 py-2 text-base font-medium',
+                  )}
+                  aria-current={item.isActive ? 'page' : undefined}
+                >
+                  {item.label}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+};
+
+export default Nav;
