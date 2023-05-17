61466f43141f6494a169561de402274601582116
Andrei Radu <raduandrei697@gmail.com>
2023-05-18 00:43:38 +0300

    signup page

--- packages/web/src/hooks/useHttpRequest/useHttp.hook.tsx
+++ packages/web/src/hooks/useHttpRequest/useHttp.hook.tsx
@@ -11,17 +11,19 @@ export interface HttpReqRes<T> {
 
 const useHttpRequest = <T,>(): HttpReqRes<T> => {
   const [data, setData] = useState<T | null>(null);
-  const [loading, setIsLoading] = useState<boolean>(true);
+  const [loading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
   const sendRequest = useCallback(async (url: string, options: AxiosRequestConfig = {}) => {
     try {
+      setIsLoading(true);
       const response: AxiosResponse<T> = await _axios(url, options);
       setData(response.data);
       return response;
     } catch (error: unknown) {
+      console.log('error axios', error);
       if (axios.isAxiosError(error)) {
-        setError(error);
+        setError(new Error(error.response?.data.message || error.response?.data.error || 'Something went wrong'));
       } else {
         setError(new Error('An unknown error occurred.'));
       }
--- packages/web/src/pages/SignUp/signup.page.tsx
+++ packages/web/src/pages/SignUp/signup.page.tsx
@@ -1,23 +1,84 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import AuthLayout from '../../components/Layouts/Auth/auth.layout';
-import { Link } from 'react-router-dom';
-import { AddressBook, Envelope, Eye } from 'phosphor-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AddressBook, Check, Envelope, Eye, Warning } from 'phosphor-react';
+import { Input } from '../../components/UI/Input/input.component';
+import useHttpRequest, { HttpReqRes } from '../../hooks/useHttpRequest/useHttp.hook';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { registerSchema, RegisterProps } from '../../schema/register.schema';
+import { AuthAccountCreated } from '../../types/auth.types';
+import TopLevelNotification from '../../components/UI/TopLevelNotification/topLevelNotification.component';
+import statsAndMaps from '../../config/mappedstats.config';
+import { PulseLoader } from 'react-spinners';
 
 const SignUp = () => {
+  const [topLevelNotification, setTopLevelNotification] = useState({
+    show: false,
+    message: '',
+    icon: <></>,
+  });
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<RegisterProps>({
+    resolver: zodResolver(registerSchema),
+    mode: 'onSubmit',
+  });
+  const { data, error, loading, sendRequest }: HttpReqRes<AuthAccountCreated> = useHttpRequest<AuthAccountCreated>();
   const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
+  const submitButtonRef = useRef<HTMLButtonElement>(null);
+  const navigate = useNavigate();
 
-  const showPassword = () => {
-    console.log('change triggered');
-    if (passwordRef.current) {
-      console.log('changed');
-      if (passwordRef.current.type === 'text') {
-        console.log('changed to password');
-        passwordRef.current.type = 'password';
+  const showPassword = (type: 'PASSWORD' | 'CONFIRM PASSWORD') => {
+    let reference = type === 'PASSWORD' ? passwordRef : confirmPasswordRef;
+
+    if (reference.current) {
+      if (reference.current.type === 'text') {
+        reference.current.type = 'password';
       } else {
-        console.log('changed to text');
-        passwordRef.current.type = 'text';
+        reference.current.type = 'text';
       }
     }
+  };
+
+  useEffect(() => {
+    if (data) {
+      submitButtonRef.current && (submitButtonRef.current.disabled = false);
+      const { isSuccess, message } = data;
+      if (isSuccess && message === statsAndMaps['accountCreatedSuccessfully'].message) {
+        setTopLevelNotification({
+          show: true,
+          message: 'Account created successfully',
+          icon: <Check className="w-14 h-8 text-green-400" />,
+        });
+
+        setTimeout(() => {
+          navigate('/login');
+        }, 5500);
+      }
+    }
+
+    if (error) {
+      submitButtonRef.current && (submitButtonRef.current.disabled = false);
+      const { message } = error;
+      setTopLevelNotification({
+        show: true,
+        message: message,
+        icon: <Warning className="w-14 h-8 text-red-500" />,
+      });
+    }
+  }, [error, data]);
+
+  const onSubmit: SubmitHandler<RegisterProps> = async (data) => {
+    submitButtonRef.current && (submitButtonRef.current.disabled = true);
+    await sendRequest('auth/local/signup', {
+      method: 'POST',
+      data: data,
+      withCredentials: false,
+    });
   };
 
   return (
@@ -36,18 +97,42 @@ const SignUp = () => {
         </>
       }
     >
-      <form className="mt-8 flex py-4 flex-col w-full  md:max-w-lg overflow-hidden">
+      {(data || error) && topLevelNotification.show && (
+        <TopLevelNotification
+          hasCloseButton={false}
+          dismissAfterXMs={5500}
+          message={topLevelNotification.message}
+          show={topLevelNotification.show}
+          onClose={() =>
+            setTopLevelNotification({
+              show: false,
+              message: '',
+              icon: <></>,
+            })
+          }
+          icon={topLevelNotification.icon}
+        />
+      )}
+
+      <form
+        id="register"
+        className="mt-8 flex flex-col w-full  md:max-w-lg overflow-hidden"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="w-full flex flex-wrap md:flex-nowrap gap-3 md:gap-5 items-center justify-between">
           {/* First name */}
           <div className="flex-1 py-1 px-1 bg-yellow-300 flex rounded-xl h-14 w-full md:max-w-[245px]">
             <div className="pl-1 flex flex-col w-4/5">
-              <label htmlFor="firstName" className="text-xs p-1">
-                First name
-              </label>
-              <input
+              <Input
+                {...register('firstName')}
+                label="First Name"
                 id="firstName"
                 type="text"
-                className="text-black border-none focus:outline-none active:outline-none bg-transparent px-1"
+                className={`text-black border-none focus:outline-none active:outline-none bg-transparent px-1 ${
+                  errors.firstName?.message && 'border-red-500'
+                }`}
+                error={errors.firstName?.message}
+                required
               />
             </div>
             <div className="flex h-full items-center justify-end w-1/5">
@@ -58,13 +143,16 @@ const SignUp = () => {
           {/* Last name */}
           <div className="flex-1 py-1 px-1 bg-yellow-300 flex rounded-xl h-14 w-full md:max-w-[245px]">
             <div className="pl-1 flex flex-col w-4/5">
-              <label htmlFor="lastName" className="text-xs p-1">
-                Last name
-              </label>
-              <input
+              <Input
+                {...register('lastName')}
+                label="Last Name"
                 id="lastName"
                 type="text"
-                className="text-black border-none focus:outline-none active:outline-none bg-transparent px-1"
+                className={`text-black border-none focus:outline-none active:outline-none bg-transparent px-1 ${
+                  errors.lastName?.message && 'border-red-500'
+                }`}
+                error={errors.lastName?.message}
+                required
               />
             </div>
             <div className="flex h-full items-center justify-end w-1/5">
@@ -74,15 +162,18 @@ const SignUp = () => {
         </div>
 
         {/* Email */}
-        <div className="flex-1 my-3 py-1 px-1 bg-yellow-300 flex rounded-xl h-14 w-full ">
+        <div className="flex-1 my-3 mt-5 py-1 px-1 bg-yellow-300 flex rounded-xl h-14 w-full ">
           <div className="pl-1 flex flex-col w-4/5">
-            <label htmlFor="email" className="text-xs p-1">
-              Email
-            </label>
-            <input
+            <Input
+              {...register('email')}
+              label="Email"
               id="email"
               type="text"
-              className="text-black border-none focus:outline-none active:outline-none bg-transparent px-1"
+              className={`text-black border-none focus:outline-none active:outline-none bg-transparent px-1 ${
+                errors.email?.message && 'border-red-500'
+              }`}
+              error={errors.email?.message}
+              required
             />
           </div>
           <div className="flex h-full items-center justify-end w-1/5">
@@ -93,26 +184,56 @@ const SignUp = () => {
         {/* Password */}
         <div className="flex-1 my-3 py-1 px-1 bg-yellow-300 flex rounded-xl h-14 w-full ">
           <div className="pl-1 flex flex-col w-4/5">
-            <label htmlFor="password" className="text-xs p-1">
-              Password
-            </label>
-            <input
-              ref={passwordRef}
+            <Input
+              {...register('password')}
+              className={`text-black border-none focus:outline-none active:outline-none bg-transparent px-1 ${
+                errors.password?.message && 'border-red-500'
+              }`}
               id="password"
               type="password"
-              className="text-black border-none focus:outline-none active:outline-none bg-transparent px-1"
+              placeholder=" "
+              autoComplete="false"
+              spellCheck="false"
+              label="Password"
+              error={errors.password?.message}
+              required
             />
           </div>
           <div className="flex h-full items-center justify-end w-1/5">
-            <Eye className="cursor-pointer w-6 h-10" color="#000" onClick={showPassword} />
+            <Eye className="cursor-pointer w-6 h-10" color="#000" onClick={showPassword.bind(this, 'PASSWORD')} />
+          </div>
+        </div>
+
+        {/*Confirm  Password */}
+        <div className="flex-1 my-3 py-1 px-1 bg-yellow-300 flex rounded-xl h-14 w-full ">
+          <div className="pl-1 flex flex-col w-4/5">
+            <Input
+              {...register('confirmPassword')}
+              className={`text-black border-none focus:outline-none active:outline-none bg-transparent px-1 ${
+                errors.confirmPassword?.message && 'border-red-500'
+              }`}
+              id="confirmPassword"
+              type="password"
+              placeholder=" "
+              autoComplete="false"
+              spellCheck="false"
+              label="Password"
+              error={errors.confirmPassword?.message}
+              required
+            />
+          </div>
+          <div className="flex h-full items-center justify-end w-1/5">
+            <Eye className="cursor-pointer w-6 h-10" color="#000" onClick={showPassword.bind(this, 'CONFIRM PASSWORD')} />
           </div>
         </div>
 
         <button
+          ref={submitButtonRef}
           type="submit"
+          form="register"
           className="my-3 bg-default-yellow text-black border-none px-2 py-4 rounded-lg shadow-sm shadow-yellow-300/50 hover:bg-default-gray hover:text-white ease-in hover:shadow-none"
         >
-          Create account
+          {loading ? <PulseLoader color="#1f1f1f" /> : 'Create account'}
         </button>
       </form>
     </AuthLayout>
