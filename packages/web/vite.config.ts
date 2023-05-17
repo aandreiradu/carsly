af2d95e91bf1306911365e33f8f8b9fa807af502
Andrei Radu <raduandrei697@gmail.com>
2023-05-18 00:42:55 +0300

    dependencies + vite port switch

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config(); // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 5173,
+  },
+});
