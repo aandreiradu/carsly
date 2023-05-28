36a135ab35bc41ab2344a17b38d547ec6a24943f
Andrei Radu <raduandrei697@gmail.com>
2023-05-29 00:01:31 +0300

    redux logger + axios credentials

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,15 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/home.page';
 import SignUp from './pages/SignUp/signup.page';
 import SignIn from './pages/SignIn/signin.page';
+// import Persist from './components/Persist/persist.component';
 
 function App() {
   return (
     <Routes>
-      <Route index path="/" element={<Home />} />
-
       {/* SignUp & SignIn */}
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
+
+      {/* <Route element={<Persist />}> */}
+      <Route index path="/" element={<Home />} />
+      {/* </Route> */}
     </Routes>
   );
 }
--- packages/web/src/api/axios/axios.ts
+++ packages/web/src/api/axios/axios.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_PORT;
+const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+axios.defaults.withCredentials = true;
 
 const _axios = axios.create({
   baseURL: BACKEND_BASE_URL,
+  withCredentials: true,
 });
 
 export default _axios;
