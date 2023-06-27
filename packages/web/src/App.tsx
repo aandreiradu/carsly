0c2fdeb9064d256af037a0b7821c878f0e2a4956
Andrei Radu <raduandrei697@gmail.com>
2023-06-28 00:32:05 +0300

    FE - route for ad page

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/home.page';
 import SignUp from './pages/SignUp/signup.page';
 import SignIn from './pages/SignIn/signin.page';
 import Persist from './components/Persist/persist.component';
+import Ad from './pages/Ad/ad.page';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
       <Route element={<Persist />}>
         <Route index path="/" element={<Home />} />
+        <Route path="/auto/add" element={<Ad title="Sell Your Car Now" />} />
       </Route>
     </Routes>
   );
