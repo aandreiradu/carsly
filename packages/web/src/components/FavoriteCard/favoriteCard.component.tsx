ea5db3619b45776bfffedf76585a2c89657d16a1
Andrei Radu <raduandrei697@gmail.com>
2023-08-20 22:45:18 +0300

    feat: favorite ads page

--- packages/web/src/components/FavoriteCard/favoriteCard.component.tsx
+++ packages/web/src/components/FavoriteCard/favoriteCard.component.tsx
@@ -1,29 +1,99 @@
-import { Heart } from 'phosphor-react';
-import { FavoriteCarAd } from '../../store/favorites/favorites.slice';
+import { Heart, Warning } from 'phosphor-react';
+import { FavoriteCarAd, setFavoriteAds, setFavoritesCount } from '../../store/favorites/favorites.slice';
+import { useCallback, useRef } from 'react';
+import useHttpRequest from '../../hooks/useHttpRequest/useHttp.hook';
+import { useDispatch } from 'react-redux';
+import { ClipLoader } from 'react-spinners';
+import TopLevelNotification, {
+  TopLevelNotificationHandlers,
+} from '../UI/TopLevelNotification/topLevelNotification.component';
 
-const FavoriteCardItem = ({ currency, name, price, thumbnail, location }: FavoriteCarAd) => {
+const FavoriteCardItem = ({ adId, currency, name, price, thumbnail, location }: FavoriteCarAd) => {
+  const topLevelNotificationRef = useRef<TopLevelNotificationHandlers>(null);
+  const { sendRequest: sendRequestFavorites, loading: loadingFavorites, error: errorFavorites } = useHttpRequest();
+  const dispatch = useDispatch();
+  const handleAddToFavorite = useCallback(async (ad: FavoriteCarAd) => {
+    const abortController = new AbortController();
+    const responseFavorites = await sendRequestFavorites('/api/ad/favorites', {
+      method: 'POST',
+      data: {
+        adId: ad.adId,
+        isOfferOfTheDay: false,
+      },
+      withCredentials: true,
+      signal: abortController.signal,
+    });
+
+    if (responseFavorites && responseFavorites.status === 200) {
+      const { count, favorites } = responseFavorites.data || {};
+      if (count && +count > 0) {
+        dispatch(setFavoritesCount(count));
+      }
+
+      if (favorites && favorites?.length) {
+        dispatch(setFavoriteAds(favorites));
+      }
+    }
+  }, []);
+
+  if (errorFavorites) {
+    if (errorFavorites instanceof Error) {
+      if (topLevelNotificationRef) {
+        topLevelNotificationRef.current?.display({
+          icon: <Warning className="w-14 h-8 text-red-600" />,
+          message: errorFavorites?.message || 'Something went wrong. Please try again later!',
+        });
+      }
+    } else {
+      if (topLevelNotificationRef) {
+        topLevelNotificationRef.current?.display({
+          icon: <Warning className="w-14 h-8 text-red-600" />,
+          message: 'Something went wrong. Please try again later!',
+        });
+      }
+    }
+  }
+
   return (
-    <div className="w-full border border-red-500 rounded-md cursor-pointer">
-      <div className="h-48 w-full">
-        <img
-          className="w-full h-full object-cover rounded-tl-md rounded-tr-md"
-          src={`http://localhost:5174/${thumbnail}` ?? ''}
-          alt="name"
-        />
-      </div>
-      <div className="flex flex-col p-4 gap-2">
-        <div className="flex items-start justify-between gap-4">
-          <p className="text-base h-10 overflow-hidden text-ellipsis leading-5">{name}</p>
-          <button className="flex w-fit items-center p-0 bg-none ">
-            <Heart weight="fill" color="red" className="h-6 w-6" />
-          </button>
+    <>
+      <TopLevelNotification ref={topLevelNotificationRef} hasCloseButton={false} dismissAfterXMs={5500} />
+      <div className="w-full border border-red-500 rounded-md cursor-pointer">
+        <div className="h-48 w-full">
+          <img
+            className="w-full h-full object-cover rounded-tl-md rounded-tr-md"
+            src={`${import.meta.env.VITE_BACKEND_URL}/${thumbnail}` ?? ''}
+            alt="name"
+          />
         </div>
-        <p className="text-sm leading-4 h-9 text-ellipsis overflow-hidden">{location}</p>
-        <p className="leading-4 text-lg text-red-500">
-          {price} {currency}
-        </p>
+        <div className="flex flex-col p-4 gap-2">
+          <div className="flex items-start justify-between gap-4">
+            <p className="text-base h-10 overflow-hidden text-ellipsis leading-5">{name}</p>
+            <button
+              disabled={loadingFavorites}
+              className="flex w-fit items-center p-0 bg-none disabled:bg-gray-500  disabled:pointer-events-none"
+              onClick={handleAddToFavorite.bind(this, {
+                adId,
+                currency,
+                name,
+                price,
+                thumbnail,
+                location,
+              })}
+            >
+              {!loadingFavorites ? (
+                <Heart weight="fill" color="red" className="h-6 w-6" />
+              ) : (
+                <ClipLoader color="#1f1f1f" size={25} />
+              )}
+            </button>
+          </div>
+          <p className="text-sm leading-4 h-9 text-ellipsis overflow-hidden">{location ?? 'Unknown'}</p>
+          <p className="leading-4 text-lg text-red-500">
+            {price} {currency}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- packages/web/src/pages/Favorites/favorites.page.tsx
+++ packages/web/src/pages/Favorites/favorites.page.tsx
@@ -10,6 +10,7 @@ import TopLevelNotification, {
 } from '../../components/UI/TopLevelNotification/topLevelNotification.component';
 import { Warning } from 'phosphor-react';
 import { Skeleton } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 interface LoadingContentProps {
   count?: number;
@@ -20,8 +21,8 @@ const LoadingContent = ({ count = 1 }: LoadingContentProps) => {
 
   return (
     <>
-      {fakeMap?.map(() => (
-        <div className="w-full md:max-w-[258px] h-[258px] flex flex-col">
+      {fakeMap?.map((_, idx) => (
+        <div key={`__skeleton_wrapper_${idx}`} className="w-full md:max-w-[258px] h-[258px] flex flex-col">
           <Skeleton variant="rectangular" width="100%" height={192} />
           <Skeleton className="mt-2" variant="text" />
           <Skeleton className="mt-2" variant="text" />
@@ -57,15 +58,24 @@ const FavoritePage = () => {
       }
     };
 
-    getFavoritesAdsByUser();
+    !favoriteAds?.length && getFavoritesAdsByUser();
   }, []);
 
-  if (errorFavoritesAds instanceof Error) {
-    if (topLevelNotificationRef) {
-      topLevelNotificationRef.current?.display({
-        icon: <Warning className="w-14 h-8 text-red-600" />,
-        message: errorFavoritesAds?.message || 'Something went wrong. Please try again later!',
-      });
+  if (errorFavoritesAds) {
+    if (errorFavoritesAds instanceof Error) {
+      if (topLevelNotificationRef) {
+        topLevelNotificationRef.current?.display({
+          icon: <Warning className="w-14 h-8 text-red-600" />,
+          message: errorFavoritesAds?.message || 'Something went wrong. Please try again later!',
+        });
+      }
+    } else {
+      if (topLevelNotificationRef) {
+        topLevelNotificationRef.current?.display({
+          icon: <Warning className="w-14 h-8 text-red-600" />,
+          message: 'Something went wrong. Please try again later!',
+        });
+      }
     }
   }
 
@@ -74,25 +84,42 @@ const FavoritePage = () => {
       <TopLevelNotification ref={topLevelNotificationRef} hasCloseButton={false} dismissAfterXMs={5500} />
       <Nav showOnAllScreens={true} setShowComponent={() => {}} />
       <section className="h-full my-6 max-h-[98%] mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <ul className="grid grid-cols-1 gap-4 xl:grid-cols-4cols-258 lg:grid-cols-3cols-258 md:grid-cols-2cols-258">
-          {loadingFavoriteAds ? (
-            <LoadingContent count={8} />
-          ) : (
-            favoriteAds.map((favItem) => (
-              <FavoriteCardItem
-                key={`${favItem.adId}`}
-                name={favItem.name}
-                adId={favItem.adId}
-                currency={favItem.currency}
-                price={favItem.price}
-                thumbnail={favItem.thumbnail}
-                location={favItem.location}
-              />
-            ))
-          )}
-        </ul>
+        {!favoriteAds.length ? (
+          <EmptyFavoriteAds />
+        ) : (
+          <ul className="grid grid-cols-1 gap-4 xl:grid-cols-4cols-258 lg:grid-cols-3cols-258 md:grid-cols-2cols-258">
+            {loadingFavoriteAds ? (
+              <LoadingContent count={8} />
+            ) : (
+              favoriteAds.map((favItem) => (
+                <FavoriteCardItem
+                  key={`${favItem.adId}`}
+                  name={favItem.name}
+                  adId={favItem.adId}
+                  currency={favItem.currency}
+                  price={favItem.price}
+                  thumbnail={favItem.thumbnail}
+                  location={favItem.location}
+                />
+              ))
+            )}
+          </ul>
+        )}
       </section>
     </>
+  );
+};
+
+const EmptyFavoriteAds = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <p className="text-lg text-ellipsis leading-6">
+        No favorites ads found. Let's add some{' '}
+        <Link className="font-bold italic text-blue-400" to={'/'}>
+          here
+        </Link>
+      </p>
+    </div>
   );
 };
 
