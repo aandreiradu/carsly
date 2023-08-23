4c7fa3bd2118a6c529c297fff44f4a0158f535eb
Andrei Radu <raduandrei697@gmail.com>
2023-08-23 22:15:32 +0300

    addToFavorite custom hook

---
+++ packages/web/src/hooks/useAddToFavorite/useAddToFavorite.hook.ts
@@ -0,0 +1,45 @@
+import { useDispatch } from 'react-redux';
+import useHttpRequest from '../useHttpRequest/useHttp.hook';
+import { useCallback } from 'react';
+import { FavoriteCarAd, setFavoriteAds, setFavoritesCount } from '../../store/favorites/favorites.slice';
+
+const useAddToFavorite = () => {
+  const dispatch = useDispatch();
+  const { sendRequest: sendRequestFavorites, loading: loadingFavorites, error: errorFavorites } = useHttpRequest();
+
+  const handleAddToFavorite = useCallback(async (ad: FavoriteCarAd) => {
+    if (loadingFavorites) return;
+
+    const abortController = new AbortController();
+    const responseFavorites = await sendRequestFavorites('/api/ad/favorites', {
+      method: 'POST',
+      data: {
+        adId: ad.adId,
+        isOfferOfTheDay: true,
+      },
+      withCredentials: true,
+      signal: abortController.signal,
+    });
+
+    if (responseFavorites && responseFavorites.status === 200) {
+      const { count, favorites } = responseFavorites.data || {};
+      if (count && +count > 0) {
+        dispatch(setFavoritesCount(count));
+      }
+
+      if (favorites && favorites?.length) {
+        dispatch(setFavoriteAds(favorites));
+      }
+    }
+
+    return responseFavorites;
+  }, []);
+
+  return {
+    handleAddToFavorite,
+    loadingFavorites,
+    errorFavorites,
+  };
+};
+
+export default useAddToFavorite;
