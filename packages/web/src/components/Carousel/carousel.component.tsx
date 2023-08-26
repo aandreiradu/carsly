4b814841b36c3c8337d4701148f9b7d9bcdf3080
Andrei Radu <raduandrei697@gmail.com>
2023-08-26 23:47:00 +0300

    feat: carousel component

--- packages/web/src/components/Carousel/carousel.component.tsx
+++ packages/web/src/components/Carousel/carousel.component.tsx
@@ -1,89 +1,82 @@
 import { useRef, useState, ReactNode, FC } from 'react';
 import { animate, motion, useMotionValue } from 'framer-motion';
+import { ArrowLeft, ArrowRight } from 'phosphor-react';
 
 export type CarouselProps = {
-  children: ReactNode;
-  className?: string;
+  imagesSource: imagesSource[];
+  classNames?: string;
 };
 
-const Carousel: FC<CarouselProps> = ({ className, children }) => {
-  const ref = useRef<HTMLUListElement>(null);
-  const [startX, setStartX] = useState(0);
-  const [scrollLeft, setScrollLeft] = useState(0);
-  const [trackMouse, setTrackMouse] = useState(false);
-  const [animationComplete, setAnimationComplete] = useState(true);
+type imagesSource = {
+  path: string;
+};
 
-  const x = useMotionValue(0);
+const Carousel = ({ imagesSource, classNames }: CarouselProps) => {
+  const [currentCarouselImage, setCurrentCarouselImage] = useState('');
+  const [carouselImagesLen, _setCarouselImagesLen] = useState(imagesSource?.length || 0);
+  const [carouselImages, _setCarouselImages] = useState<typeof imagesSource>([]);
+  const [carouselCurrentIndex, setCarouselCurrentIndex] = useState(0);
 
-  const handleMouseMove = (e: React.PointerEvent<HTMLUListElement>) => {
-    if (!ref.current) return;
-    if (!trackMouse) return;
+  const handleImageChange = (direction: 'LEFT' | 'RIGHT') => {
+    switch (direction) {
+      case 'RIGHT': {
+        if (+carouselCurrentIndex + 1 <= carouselImagesLen) {
+          setCarouselCurrentIndex((prev) => +prev + 1);
+          setCurrentCarouselImage(carouselImages[carouselCurrentIndex].path);
+        } else {
+          setCarouselCurrentIndex(1);
+          setCurrentCarouselImage(carouselImages[0].path);
+        }
 
-    setAnimationComplete(false);
+        return;
+      }
 
-    const xVal = e.pageX - ref.current.offsetLeft;
-    const walk = (xVal - startX) * 2;
+      case 'LEFT': {
+        if (carouselCurrentIndex === 1) {
+          setCarouselCurrentIndex(carouselImagesLen);
+          setCurrentCarouselImage(carouselImages[carouselImagesLen - 1].path);
+        } else {
+          setCurrentCarouselImage(carouselImages[carouselCurrentIndex - 2].path);
+          setCarouselCurrentIndex((prev) => +prev - 1);
+        }
+        return;
+      }
 
-    const controls = animate(x, scrollLeft - walk, {
-      type: 'tween',
-      ease: 'easeInOut',
-      duration: 0.5,
-      onUpdate: (val) => {
-        if (!ref.current) return;
-        ref.current.scrollLeft = val;
-      },
-      onComplete: () => {
-        setAnimationComplete(true);
-      },
-      onStop: () => {
-        setAnimationComplete(true);
-      },
-    });
-    return controls.stop;
-  };
-
-  const handleMouseDown = (e: React.PointerEvent<HTMLUListElement>) => {
-    // if (!(e.target instanceof HTMLLIElement)) return;
-    if (!ref.current) return;
-
-    setTrackMouse(true);
-
-    const startX = e.pageX - ref.current.offsetLeft;
-    setStartX(startX);
-
-    const scrollLeft = ref.current.scrollLeft;
-    setScrollLeft(scrollLeft);
-  };
-
-  const handleMouseLeave = () => {
-    setTrackMouse(false);
-  };
-
-  const handleMouseUp = () => {
-    setTrackMouse(false);
-  };
-
-  const handleScroll = () => {
-    if (!ref.current) return;
-
-    if (animationComplete) {
-      x.set(ref.current.scrollLeft);
+      default: {
+        console.log('unhandled direction');
+      }
     }
   };
 
   return (
-    <motion.ul
-      ref={ref}
-      className={className}
-      onMouseMove={handleMouseMove}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseLeave}
-      onScroll={handleScroll}
-    >
-      {children}
-    </motion.ul>
+    <div className="flex flex-col  relative">
+      <div className="w-full bg-yellow-400 flex items-center justify-center">
+        <ArrowLeft
+          onClick={handleImageChange.bind(this, 'LEFT')}
+          className="absolute top-1/2 left-0 -translate-y-1/2"
+          height={'30px'}
+          width={'60px'}
+        />
+        <img className="h-[445px] w-[850px] bg-cover" src={`${import.meta.env.VITE_BACKEND_URL}/${currentCarouselImage}`} />
+        <ArrowRight
+          onClick={handleImageChange.bind(this, 'RIGHT')}
+          className="absolute top-1/2 right-0 -translate-y-1/2"
+          height={'30px'}
+          width={'60px'}
+        />
+      </div>
+      <button className="absolute bottom-1/4 right-6 p-1">{`${carouselCurrentIndex} / ${carouselImagesLen}`}</button>
+      <ul className="w-full  bg-green-400 flex items-center gap-3 overflow-y-auto">
+        {/* {info?.images.map((img, index) => ( */}
+        {carouselImages.map((img, index) => (
+          <img
+            className="h-32 overflow-hidden flex-shrink-0"
+            key={`${index}__`}
+            src={`${import.meta.env.VITE_BACKEND_URL}/${img.path}`}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
-
 export default Carousel;
