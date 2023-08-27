75e7eef0fe4822277c3e541cbb99eaa9d19df11d
Andrei Radu <raduandrei697@gmail.com>
2023-08-27 23:42:37 +0300

    feat: ad  technical details component

---
+++ packages/web/src/components/Ad/adTechnicalDetails.component.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { Ad } from '../../types/ad.types';
+import { CaretDown, CaretUp } from 'phosphor-react';
+
+const AdTechnicalDetails = (props: Ad) => {
+  const [showFullDetais, setShowFullDetails] = useState<boolean>(false);
+  return (
+    <div className={`w-full  md:w-1/2 overflow-hidden flex flex-col`}>
+      <p className="my-2 font-bold leading-4 text-base px-2 md:px-0">Details</p>
+      <div
+        className={`grid grid-cols-2 ${
+          !showFullDetais ? 'h-[200px]' : 'h-[600px]'
+        } overflow-hidden animate-expendHeight transition-height px-2 md:px-0`}
+      >
+        <div className="flex flex-col gap-1">
+          <p className="overflow-hidden text-sm h-8">Brand</p>
+          <p className="overflow-hidden text-sm h-8">Model</p>
+          <p className="overflow-hidden text-sm h-8">Year</p>
+          <p className="overflow-hidden text-sm h-8">KM</p>
+          <p className="overflow-hidden text-sm h-8">Fuel</p>
+          <p className="overflow-hidden text-sm h-8">Engine size</p>
+          <p className="overflow-hidden text-sm h-8">Transmission</p>
+          <p className="overflow-hidden text-sm h-8">Polluation norm</p>
+          <p className="overflow-hidden text-sm h-8">Gearbox</p>
+          <p className="overflow-hidden text-sm h-8">Body type</p>
+          <p className="overflow-hidden text-sm h-8">Doors</p>
+          <p className="overflow-hidden text-sm h-8">Color</p>
+          <p className="overflow-hidden text-sm h-8">Seats</p>
+          <p className="overflow-hidden text-sm h-8">Color type</p>
+          <p className="overflow-hidden text-sm h-8">Registered</p>
+          <p className="overflow-hidden text-sm h-8">Without accident</p>
+          <p className="overflow-hidden text-sm h-8">Service card</p>
+        </div>
+        <div className="flex flex-col gap-1 ">
+          <p className="overflow-hidden text-sm h-8">{props?.carBrandId}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.carModelId}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.year}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.KM}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.fuelType}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.engineSize}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.transmission}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.polluationNorm}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.gearbox}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.bodyType}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.noOfDoors}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.color}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.seats}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.colorType}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.isRegistered ? 'Yes' : 'No'}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.isWithoutAccident ? 'Yes' : 'No'}</p>
+          <p className="overflow-hidden text-sm h-8">{props?.isServiceCardAvailable ? 'Yes' : 'No'}</p>
+        </div>
+      </div>
+
+      <button
+        onClick={() => setShowFullDetails((prev) => !prev)}
+        className="w-36 h-9 flex items-center justify-center gap-1 my-1 text-sm text-blue-500"
+      >
+        {!showFullDetais ? `Show full details` : `Hide details`}
+        {!showFullDetais ? <CaretDown /> : <CaretUp />}
+      </button>
+    </div>
+  );
+};
+
+export default AdTechnicalDetails;
